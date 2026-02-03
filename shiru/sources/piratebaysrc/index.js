deb3458d72951ab9522aeb967629920f7ff777e8
Emekalim <nwaezeigwefranklin@yahoo.com>
2026-02-03 13:30:41 -0500

    fixing error

--- shiru/sources/piratebaysrc/index.js
+++ shiru/sources/piratebaysrc/index.js
@@ -41,10 +41,12 @@ export default new class Nyaa extends AbstractSource {
    */
   async _search(title, episode) {
     let query = title.replace(/[^\w\s-]/g, " ").trim()
-    if (episode) query += ` ${episode.toString().padStart(2, "0")}`
+    // if (episode) query += ` ${episode.toString().padStart(2, "0")}`
 
     const url = this.base + encodeURIComponent(query)
+    console.log("url:", url)
     const res = await fetch(url)
+    
     if (!res.ok) return []
 
     const data = await res.json()
